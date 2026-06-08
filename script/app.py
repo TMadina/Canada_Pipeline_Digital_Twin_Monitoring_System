7b6e4a13c5d71624b344378490403ec35df4a587
Madina <tmadina1411@gmail.com>
2026-06-09 00:22:34 +0500

    Update app.py

--- script/app.py
+++ script/app.py
@@ -5,12 +5,12 @@ import streamlit as st
 import time
 from sqlalchemy import create_engine, text
 
-
+#упаковать в отдельный файл данные
 DB_USER = "postgres"
-DB_PASS = "102030"
+DB_PASS = ""
 DB_HOST = "localhost"
 DB_PORT = "5432"
-DB_NAME = "fort_mcmurray_db"
+DB_NAME = ""
 
 connection_string = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(connection_string)
